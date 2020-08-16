c78a22d47361408e170b92fdc857e16e73b401fc
Ayush <ayushrj1516@gmail.com>
2020-08-16 12:31:33 +0530

    minor changes

---
+++ src/com/company/WeekSwitchMethod.java
@@ -0,0 +1,41 @@
+package com.company;
+
+
+import java.util.Scanner;
+
+public class WeekSwitchMethod {
+
+    public static void main(String[] args) {
+        Scanner weekday = new Scanner(System.in);
+        System.out.println("Enter the keyword: ");
+        int username = weekday.nextInt();
+
+        switch (username){
+            case 1:
+                System.out.println("the weekday is Monday");
+                break;
+            case 2:
+                System.out.println("the weekday is Tuesday");
+                break;
+            case 3:
+                System.out.println("the weekday is Wednesday");
+                break;
+            case 4:
+                System.out.println("the weekday is Thrusday");
+                break;
+            case 5:
+                System.out.println("the weekday is Friday");
+                break;
+            case 6:
+                System.out.println("the weekday is Saturday");
+                break;
+            case 7:
+                System.out.println("the weekday is Sunday");
+                break;
+            default:
+                System.out.println("Please check your input");
+
+
+        }
+    }
+}
