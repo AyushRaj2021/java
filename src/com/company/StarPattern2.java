5ab14826e5d8f822ff2770b0e2e7db65d318c2cc
Ayush <ayushrj1516@gmail.com>
2020-08-16 21:49:54 +0530

    minor change

---
+++ src/com/company/StarPattern2.java
@@ -0,0 +1,17 @@
+package com.company;
+
+import java.util.Scanner;
+
+public class StarPattern2 {
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter the value of n:");
+        int n = sc.nextInt();
+        for(int j=1; j<=n; j++){
+            for(int i=1; i<=j; i++){
+                System.out.print("* ");
+            }
+            System.out.println();
+        }
+    }
+}
