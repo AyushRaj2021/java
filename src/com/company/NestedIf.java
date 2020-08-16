0cea53ff44116115b6bcc5a8824f180ed9ea1850
Ayush <ayushrj1516@gmail.com>
2020-08-16 13:09:32 +0530

    minor change

--- src/com/company/NestedIf.java
+++ src/com/company/NestedIf.java
@@ -1,10 +1,18 @@
 package com.company;
 
+import java.util.Scanner;
+
 public class NestedIf {
     public static void main(String[] args) {
-        int a = 18,b = 126, c = 72;
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the value of a : ");
+        int a = sc.nextInt();
+        System.out.println("Enter the value of b :");
+        int b = sc.nextInt();
+        System.out.println("Enter the value of c :");
+        int c = sc.nextInt();
         int result = 0;
         result = a > b ? a > c ? a : c : b > c ? b : c;
-        System.out.println(result);
+        System.out.println("the largest of all number is " + result);
     }
 }
