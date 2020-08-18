ba2aa0297a21c02508a0cdd047b9d440e9e1e5fc
Ayush <ayushrj1516@gmail.com>
2020-08-18 22:43:31 +0530

    new implementation

--- src/array/AagramOfString.java
+++ src/array/AagramOfString.java
@@ -6,7 +6,23 @@ public class AagramOfString {
         String b = "aba";
         boolean isAnagram = true;
         int al [] = new int [256];
-        int bl [] = new int [256];
+//        int bl [] = new int [256];
+//
+//        for(char c: a.toCharArray()) {
+//            int index = (int) c;
+//            al[index]++;
+//        }
+//        for(char c: b.toCharArray()) {
+//            int index = (int) c;
+//            bl[index]++;
+//        }
+//        for (int i =0; i<256;i++){
+//            if (al[i] != bl[i]) {
+//                isAnagram = false;
+//                break;
+//            }
+//        }
+
 
         for(char c: a.toCharArray()) {
             int index = (int) c;
@@ -14,10 +30,10 @@ public class AagramOfString {
         }
         for(char c: b.toCharArray()) {
             int index = (int) c;
-            bl[index]++;
+            al[index]--;
         }
         for (int i =0; i<256;i++){
-            if (al[i] != bl[i]) {
+            if (al[i] != 0) {
                 isAnagram = false;
                 break;
             }
